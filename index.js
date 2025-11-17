03c5ab14df83a49aa5a65122f5ea2d95a752c3ef
Yurij Yatskiv <yurayatskiv07@gmail.com>
2025-11-17 16:06:09 +0200

    Add PATCH method to CORS options

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 const corsOptions = {
   origin: "https://taskhub-6upa.onrender.com", 
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
   preflightContinue: false,
@@ -69,6 +69,7 @@ async function startApp() {
     }
 }
 startApp();
+
 
 
 
